b5744bb35b0daae5dacc64062038e34bd4ad1912
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-19 16:03:01 +0600

    deploy sus

--- client/src/api/Instance.ts
+++ client/src/api/Instance.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://shopping-cart-server-eta.vercel.app'
+    // baseURL: 'http://localhost:3000'
 })
 export default instance;
